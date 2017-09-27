85275db8d64e11909619f80a29133c2979823c9e
bbutner <bbutner@students.dacc.edu>
2017-09-26 21:42:15 -0500

    Gotta close that connection

--- LogKeeper/Main.cs
+++ LogKeeper/Main.cs
@@ -92,6 +92,7 @@ namespace LogKeeper
                 finally
                 {
                     reader.Close();
+                    tempCon.Close();
                 }
             }
             else
